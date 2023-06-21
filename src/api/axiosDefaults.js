61306e7d0e8726ade855635b3350e1e931586c22
Johneriksson88 <john.e.eriksson@gmail.com>
2023-06-21 10:33:25 +0000

    Add sign in/up functionality, connect to APIand create user context

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://beatbuddies-api-a72df4dfc93e.herokuapp.com/';
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.withCredentials = true;
