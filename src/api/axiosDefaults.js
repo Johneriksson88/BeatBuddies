183fddd2385246fb77548996f9229dc0912ba441
Johneriksson88 <john.e.eriksson@gmail.com>
2023-06-21 22:11:33 +0000

    Add axios interceptors to refresh tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://beatbuddies-api-a72df4dfc93e.herokuapp.com/';
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
