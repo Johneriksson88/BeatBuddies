8937c29c5c20f1d4a11dab4ac578e57aad05b59e
Johneriksson88 <john.e.eriksson@gmail.com>
2023-07-18 22:30:59 +0000

    Additional styling of moodpage

--- src/pages/moods/MoodPage.js
+++ src/pages/moods/MoodPage.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { axiosReq } from "../../api/axiosDefaults";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
-import { Col, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import Asset from "../../components/Asset";
 import Post from "../posts/Post";
 import PopularProfiles from "../profiles/PopularProfiles";
+import appStyles from "../../App.module.css";
 
 function MoodPage() {
   const { id } = useParams();
@@ -25,20 +26,36 @@ function MoodPage() {
 
     fetchMood();
   }, [id]);
-
   return (
-    <Row className="h-100">
+    <Row>
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <h2>{mood.name}</h2>
-        {hasLoaded ? (
-          <Col>
-            {posts.map((post) => (
-              <Post key={post} {...post} setPosts={setPosts} />
-            ))}
-          </Col>
-        ) : (
-          <Asset spinner />
-        )}
+        <Container className={appStyles.Content}>
+          <Row noGutters className="px-3 d-flex justify-content-center">
+          
+            <Col lg={6} className="text-center">
+              <h3 className="m-2">{mood.name}</h3>
+              <hr></hr>
+              <Row className="justify-content-center no-gutters">
+                <Col xs={3} className="my-2">
+                  <div>{posts.length}</div>
+                  <div>posts</div>
+                </Col>
+              </Row>
+            </Col>
+
+            
+            <hr></hr>
+          </Row>
+          {hasLoaded ? (
+            <Col>
+              {posts.map((post) => (
+                <Post key={post} {...post} setPosts={setPosts} />
+              ))}
+            </Col>
+          ) : (
+            <Asset spinner />
+          )}
+        </Container>
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
         <PopularProfiles />
--- src/pages/moods/MoodsPage.js
+++ src/pages/moods/MoodsPage.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { axiosReq } from "../../api/axiosDefaults";
-import { Button, Col, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import Asset from "../../components/Asset";
 import styles from "../../styles/MoodsPage.module.css";
+import appStyles from "../../App.module.css";
 
 function MoodsPage() {
   const [moods, setMoods] = useState([]);
@@ -30,22 +31,30 @@ function MoodsPage() {
           <i className={`far fa-plus-square ${styles.plus}`}></i>New mood
         </Button>
       </Row>
-      <Col className="py-2 p-0 p-lg-2 d-inline" lg={8}>
-        {hasLoaded ? (
-          <>
-            {moods.map((mood) => (
-              <Link to={`/moods/${mood.id}`}>
-                <Button size="lg" className="m-2" key={mood.id}>
-                  {mood.name}
-                </Button>
-              </Link>
-            ))}
-          </>
-        ) : (
-          <Asset spinner />
-        )}
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <Container className={appStyles.Content}>
+          <Col className="py-2 p-0 p-lg-2 d-inline" lg={8}>
+            {hasLoaded ? (
+              <>
+                {moods.map((mood) => (
+                  <Link to={`/moods/${mood.id}`}>
+                    <Button size="lg" className="m-2" key={mood.id}>
+                      {mood.name}
+                    </Button>
+                  </Link>
+                ))}
+              </>
+            ) : (
+              <Asset spinner />
+            )}
+          </Col>
+        </Container>
       </Col>
-      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+
+      <Col
+        lg={4}
+        className="d-none d-lg-block p-0 p-lg-2 text-center"
+      >
         <Link to={"/createmood"}>
           <Button variant="warning">
             <i className={`far fa-plus-square ${styles.plus}`}></i>New mood
